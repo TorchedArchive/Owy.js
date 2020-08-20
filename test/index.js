1aca8d6e6f6b8d1cc408b3c5f9fc029edd88f26c
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-08-19 22:20:19 -0400

    0.2.3 - newline after symbol function

--- test/index.js
+++ test/index.js
@@ -2,4 +2,11 @@ const owy = require('../lib');
 const spinner = new owy.Spinner('Checking your vibe', {style: {stages: ['-', '+'] } });
 
 spinner.start();
-setTimeout(() => spinner.success('You passed the vibe check!'), 4000);
+setTimeout(() => {
+	spinner.style = owy.styles.line;
+	spinner.success('You passed the vibe check!').start('Downloading cat images');
+	setTimeout(() => {
+		spinner.dual = true;
+		spinner.info('Downloaded! One of the images were too cute.').start('Compressing an image..');
+	}, 4000);
+}, 4000);
